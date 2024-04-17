7b6f141ed2228c0fa24a16758f7382d57e8609e8
W1adish <162422017+W1adish@users.noreply.github.com>
2024-04-17 09:05:29 +0200

    Technology.java

--- Technology.java
+++ Technology.java
@@ -1,12 +1,10 @@
 public class Technology {
         private String name;
         private int bonus;
-        // Constructor
         public Technology(String name, int bonus) {
             this.name = name;
             this.bonus = bonus;
         }
-        // Getter methods
         public String getName() {
             return name;
         }
